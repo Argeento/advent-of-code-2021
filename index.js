cbfdfdf32d33cb5284cbf8ae94a8bcec4fa3477f
argeento <adrian.wieprzkowicz@gmail.com>
2021-12-01 21:47:12 +0100

    Init

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const dayNr = process.argv[2] || '1'
+const partNr = process.argv[3] || '1'
+
+require('ts-node').register()
+
+console.log(`Running day-${dayNr} part-${partNr}...`)
+
+require(path.resolve(
+  __dirname,
+  `day-${dayNr.padStart(2, '0')}`,
+  `part-${partNr}.ts`
+))
---
+++ utils.ts
@@ -0,0 +1,89 @@
+import { readFileSync } from 'fs'
+
+interface Array2d<T> extends Array<T[]> {}
+
+export interface Position {
+  x: number
+  y: number
+}
+
+export interface Position3d extends Position {
+  z: number
+}
+
+export interface Point extends Position {}
+export interface Point3d extends Position3d {}
+
+export function getLinesFromFile(file: string): string[] {
+  return readFileSync(file).toString().split('\n')
+}
+
+export function getNumbersFromFile(file: string): number[] {
+  return getLinesFromFile(file).map(line => parseFloat(line))
+}
+
+export function add(a: number, b: number): number {
+  return a + b
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b
+}
+
+export function asc(a: any, b: any): number {
+  return a - b
+}
+
+export function desc(a: any, b: any): number {
+  return b - a
+}
+
+export function isInRange(min: number, value: number, max: number): boolean {
+  return value >= min && value <= max
+}
+
+export function deepCopy<T>(obj: T): T {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+export function rotateClockwise<T>(arr: Array2d<T>): Array2d<T> {
+  const copy = deepCopy<Array2d<T>>(arr)
+  const n = copy.length
+  const x = Math.floor(n / 2)
+  const y = n - 1
+
+  for (let i = 0; i < x; i++) {
+    for (let j = i; j < y - i; j++) {
+      const tmp = copy[i][j]
+      copy[i][j] = copy[y - j][i]
+      copy[y - j][i] = copy[y - i][y - j]
+      copy[y - i][y - j] = copy[j][y - i]
+      copy[j][y - i] = tmp
+    }
+  }
+
+  return copy
+}
+
+export function flipX<T>(arr: Array2d<T>): Array2d<T> {
+  let copy = deepCopy<Array2d<T>>(arr)
+  copy = copy.map(row => row.reverse())
+
+  return copy
+}
+
+export function flipY<T>(arr: Array2d<T>): Array2d<T> {
+  let copy = deepCopy<Array2d<T>>(arr)
+  copy = copy.reverse()
+
+  return copy
+}
+
+export function getManhatanDistance(
+  positionA: Position,
+  positionB: Position = { x: 0, y: 0 }
+): number {
+  return (
+    Math.abs(positionA.x - positionB.x) + Math.abs(positionA.y - positionB.y)
+  )
+}
